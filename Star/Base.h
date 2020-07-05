9087c9cfb27cf7f9748fe7595c457cda3671fd0d
panyz <592744797@qq.com>
2020-07-06 00:42:09 +0800

    [feature]:使用新的分支管理

--- Star/Base.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-
-#define STAR_NAMESPACE_BEGIN namespace star {
-#define STAR_NAMESPACE_END };
--- Star/tools/Tools.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <string>
-
-bool readFileData(std::string& file_path, std::string& out_data);
-
-std::string getCurrentPath();
